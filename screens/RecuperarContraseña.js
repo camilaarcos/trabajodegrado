3c7d4f58a25c5d74a20642904b2193065d597af7
camilaarcos <maria.arcos@correounivalle.edu.co>
2024-09-05 18:22:12 -0500

    estilos

--- screens/RecuperarContraseña.js
+++ screens/RecuperarContraseña.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { View, Text, TextInput, ScrollView, TouchableOpacity, StyleSheet, Alert, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { validateEmail } from '../utils/Ayudas';
 import { passwordReset } from '../utils/Acciones';
@@ -41,7 +42,20 @@ export default function RecoverPassword  () {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Recuperar Contraseña</Text>
+      <Image source={require('../assets/fondo.png')} style={[styles.imagefondo, StyleSheet.absoluteFill]} />
+      
+      <ScrollView contentContainerStyle={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        marginTop: 90,
+      }}>
+        
+        <BlurView intensity={100} style={styles.blurPrincipal}>   
+      <View style={styles.login1}>
+      <Text style={styles.tittle}>Recuperar Contraseña</Text>
+      <Image source={require('../assets/Security.png')} style={styles.avatar} />
       <TextInput
         style={styles.input}
         placeholder="Correo electrónico"
@@ -54,6 +68,9 @@ export default function RecoverPassword  () {
       <TouchableOpacity onPress={handleRecoverPassword} style={styles.button}>
         <Text style={styles.buttonText}>Recuperar Contraseña</Text>
       </TouchableOpacity>
+      </View>
+      </BlurView>
+        </ScrollView>
     </View>
   );
 };
@@ -61,30 +78,65 @@ export default function RecoverPassword  () {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
+  blurPrincipal: {
+    height: '80%',
+    borderRadius: 10,
+    overflow: 'hidden',
+  },
+  imagefondo: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  login1: {
+    width: 350,
+    height: '100%',
+    borderColor: '#4d82bc',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  tittle: {
+    marginTop: 60,
+    fontSize: 20,
+    color: '#4d82bc',
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    // borderRadius: 50,
+    marginBottom: 10,
   },
   input: {
-    width: '100%',
-    padding: 8,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 4,
-    marginBottom: 16,
+    width: 250,
+    height: 40,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20,
+    fontWeight: '400',
+    alignSelf: 'center',
   },
   button: {
-    backgroundColor: '#007BFF',
-    padding: 12,
-    borderRadius: 4,
+    backgroundColor: "#ffffff80",
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#fff',
+    margin: 10,
   },
   buttonText: {
-    color: '#fff',
-    fontSize: 16,
+    color: "#4d82bc",
+      fontWeight: 'bold',
   },
+  
 });
 
