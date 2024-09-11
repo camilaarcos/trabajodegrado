646e69729960fc0fe458aecb8e5003fa95cfd189
camilaarcos <maria.arcos@correounivalle.edu.co>
2024-09-10 23:04:18 -0500

    mejoras de barrios

--- screens/Registro.js
+++ screens/Registro.js
@@ -5,6 +5,10 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import {database} from '../src/config/firebase';
 import {collection, addDoc} from 'firebase/firestore';
 import { useNavigation } from "@react-navigation/native";
+import SelectDropdown from 'react-native-select-dropdown';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Verifica que el nombre del paquete y el ícono sean correctos
+
 import { isEmpty, size } from 'lodash'
 
 export default function Registro() {
@@ -19,6 +23,154 @@ export default function Registro() {
   const [Fecha, setFecha] = useState(new Date());
   const [showDatePicker, setShowDatePicker] = useState(false);
 
+  const data = [
+    { title: "Accidente de Tránsito", icon: "car" },
+    { title: "Acoso Sexual", icon: "ban" },
+    { title: "Amenazas", icon: "exclamation-triangle" },
+    { title: "Extorsión", icon: "money" },
+    { title: "Homicidio", icon: "user-times" },
+    { title: "Hurto", icon: "hand-paper-o" },
+    { title: "Suicidio", icon: "meh-o" },
+    { title: "Violencia", icon: "hand-rock-o" },
+    { title: "Violencia Intrafamiliar", icon: "home" },
+  ];
+
+  const dataBarrio = [
+    { title: "Agua clara" },
+    { title: "Aguirre" },
+    { title: "Alameda" },
+    { title: "Alameda II" },
+    { title: "Alvernia" },
+    { title: "Asoagrin" },
+    { title: "Bello Horizonte" },
+    { title: "Avenida Cali" },
+    { title: "Bolivar" },
+    { title: "Bosques de Maracaibo" },
+    { title: "Buenos Aires" },
+    { title: "Casa Huertas" },
+    { title: "Centro" },
+    { title: "Cespedes" },
+    { title: "Chiminangos" },
+    { title: "Ciudad Campestre" },
+    { title: "Ciudad Las Palmas" },
+    { title: "Comfamiliar" },
+    { title: "Comuneros" },
+    { title: "Conjunto La Villa" },
+    { title: "Conjunto Lusitania" },
+    { title: "Corazon del Valle" },
+    { title: "Departamental" },
+    { title: "Diablos Rojos" },
+    { title: "Diablos Rojos II" },
+    { title: "Doce de octubre" },
+    { title: "Dorado" },
+    { title: "El Bosque" },
+    { title: "El Bosquecito" },
+    { title: "El Condor" },
+    { title: "El Condor II" },
+    { title: "El Descanso" },
+    { title: "El Lago" },
+    { title: "El Laguito" },
+    { title: "El Limonar" },
+    { title: "El Palmar" },
+    { title: "El Paraiso" },
+    { title: "El Pinar" },
+    { title: "El Refugio" },
+    { title: "El Retiro" },
+    { title: "Entre Rios" },
+    { title: "Escobar" },
+    { title: "Esperanza" },
+    { title: "Estambul" },
+    { title: "Farfan" },
+    { title: "Fatima" },
+    { title: "Flor de la Campana" },
+    { title: "Franciscanos" },
+    { title: "Guayacanes" },
+    { title: "Horizonte" },
+    { title: "Internacional" },
+    { title: "Jardin" },
+    { title: "Jazmin" },
+    { title: "Jorge Eliecer Gaitan" },
+    { title: "José Antonio Galan" },
+    { title: "Juan de Lemus" },
+    { title: "Juan XXIII" },
+    { title: "La Bastilla" },
+    { title: "La Campiña" },
+    { title: "La Ceiba" },
+    { title: "La Graciela" },
+    { title: "La Independencia" },
+    { title: "La Inmaculada" },
+    { title: "La Merced" },
+    { title: "La Paz" },
+    { title: "La Quinta" },
+    { title: "La Rivera" },
+    { title: "La Santa Cruz" },
+    { title: "La Trinidad" },
+    { title: "Las Acacias" },
+    { title: "Las Americas" },
+    { title: "Las Brisas" },
+    { title: "Las Delicias" },
+    { title: "Las Nieves" },
+    { title: "Las Olas" },
+    { title: "Las Veraneras" },
+    { title: "Laureles" },
+    { title: "Laureles II" },
+    { title: "Los Olmos" },
+    { title: "Los Tolues" },
+    { title: "Maracaibo" },
+    { title: "Marandua" },
+    { title: "Maria Clara" },
+    { title: "Miraflores" },
+    { title: "Morales" },
+    { title: "Moralito" },
+    { title: "Municipal" },
+    { title: "Nariño" },
+    { title: "Nuevo Alvernia" },
+    { title: "Nuevo Morales" },
+    { title: "Nuevo Principe" },
+    { title: "Palobonito" },
+    { title: "Panamericano" },
+    { title: "Playas" },
+    { title: "Popular" },
+    { title: "Portales del Rio" },
+    { title: "Prados del Norte" },
+    { title: "Primero de Mayo" },
+    { title: "Principe" },
+    { title: "Progresar" },
+    { title: "Pueblo Nuevo" },
+    { title: "Quintas de San Felipe" },
+    { title: "Rio Paila" },
+    { title: "Rojas" },
+    { title: "Ruben Cruz Velez" },
+    { title: "Sajonia" },
+    { title: "Salesianos" },
+    { title: "Saman del Norte" },
+    { title: "San Antonio" },
+    { title: "San Benito" },
+    { title: "San Carlos" },
+    { title: "San Luis" },
+    { title: "San Marino" },
+    { title: "San Pedro Claver" },
+    { title: "San Vicente de Paul" },
+    { title: "Santa Isabel" },
+    { title: "Santa Rita del Rio" },
+    { title: "Senderos Villa Liliana" },
+    { title: "Siete de Agosto" },
+    { title: "Tercer Milenio" },
+    { title: "Tomas Uribe Uribe" },
+    { title: "Urbanizacion Lomitas" },
+    { title: "Urbanizacion Peñaranda" },
+    { title: "Urbanizacion Santa Lucia" },
+    { title: "Victoria" },
+    { title: "Villa Campestre" },
+    { title: "Villa Colombia" },
+    { title: "Villa del Lago" },
+    { title: "Villa del Rio" },
+    { title: "Villa del Sur" },
+    { title: "Villa Liliana" },
+    { title: "Villanueva" },
+
+
+  ];
 
 
   const validateForm = () => {
@@ -69,6 +221,7 @@ export default function Registro() {
     }
   };
 
+
 return(
     <ScrollView contentContainerStyle={{
       flexGrow: 1,
@@ -80,26 +233,34 @@ return(
         <Text style={styles.tittle}>Registro de crímenes</Text>
         <Image source={require('../assets/Info.png')} style={styles.imageStyle} />
         <Text style={styles.texto}>Seleccione el tipo de crímen</Text>
-        <View style={styles.pickerContainer}>
-        <Picker
-        selectedValue={newItem.Tipo}
-        style={styles.picker}
-        onValueChange={(itemValue) => setNewItem({ ...newItem, Tipo: itemValue })}
-        itemStyle={styles.pickerItem}
-      >
-        <Picker.Item label="          Tipo de crímen" value="" />
-        <Picker.Item label="Accidente de tránsito" value="Accidente de Tránsito" />
-        <Picker.Item label="Acoso Sexual" value="Acoso Sexual" />
-        <Picker.Item label="Amenazas" value="Amenazas" />
-        <Picker.Item label="Extorsión" value="Extorsión" />
-        <Picker.Item label="Homicidio" value="Homicidio" />
-        <Picker.Item label="Hurto" value="Hurto" />
-        <Picker.Item label="Suicidio" value="Suicidio" />
-        <Picker.Item label="Violencia" value="Violencia" />
-        <Picker.Item label="Violencia Intrafamiliar" value="Violencia Intrafamiliar" />
+
         
-      </Picker>
-      </View>
+        <SelectDropdown
+        data={data}
+        onSelect={(selectedItem) => {
+          setNewItem({ ...newItem, Tipo: selectedItem.title });
+        }}
+        renderButton={(selectedItem, isOpened) => (
+          <View style={styles.dropdownButtonStyle}>
+            {selectedItem && (
+              <Icon name={selectedItem.icon} style={styles.dropdownButtonIconStyle} />
+            )}
+            <Text style={styles.dropdownButtonTxtStyle}>
+              {(selectedItem && selectedItem.title) || "Tipo de crimen"}
+            </Text>
+            <Icon name={isOpened ? "chevron-up" : "chevron-down"} style={styles.dropdownButtonArrowStyle} />
+          </View>
+        )}
+        renderItem={(item, isSelected) => (
+          <View style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
+            <Icon name={item.icon} style={styles.dropdownItemIconStyle} />
+            <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+          </View>
+        )}
+        showsVerticalScrollIndicator={true}
+        dropdownStyle={styles.dropdownMenuStyle}
+      />
+      
       <Text style={styles.texto}>Seleccione la fecha del crímen</Text>
       {showDatePicker && (
         <DateTimePicker
@@ -125,9 +286,35 @@ return(
         <TextInput 
         onChangeText={(text)=> setNewItem({...newItem, Direccion: text})}
         style={styles.inputContainer} placeholder="Dirección" require/>
-        <TextInput
-        onChangeText={(text)=> setNewItem({...newItem, Barrio: text})}
-        style={styles.inputContainerbarrio} placeholder="Barrio" require/>
+        <SelectDropdown
+        data={dataBarrio}
+        onSelect={(selectedItem) => {
+          setNewItem({ ...newItem, Barrio: selectedItem.title });
+        }}
+        renderButton={(selectedItem, isOpened) => (
+          <View style={styles.dropdownButtonStyle}>
+            <Text style={styles.dropdownButtonTxtStyle}>
+              {(selectedItem && selectedItem.title) || "Barrio"}
+            </Text>
+            <Icon name={isOpened ? "chevron-up" : "chevron-down"} style={styles.dropdownButtonArrowStyle} />
+          </View>
+        )}
+        renderItem={(item, index, isSelected) => (
+          <View style={{ ...styles.dropdownItemStyle, ...(isSelected && { backgroundColor: '#D2D9DF' }) }}>
+            <Text style={styles.dropdownItemTxtStyle}>{item.title}</Text>
+          </View>
+        )}
+        showsVerticalScrollIndicator={true}
+        dropdownStyle={styles.dropdownMenuStyle}
+        search
+        searchInputStyle={styles.dropdownItemStyle}
+        searchInputTxtColor={'#151E26'}
+        searchPlaceHolder={'Buscar Barrio'}
+        searchPlaceHolderColor={'#72808D'}
+        renderSearchInputLeftIcon={() => {
+          return <FontAwesome name={'search'} color={'#72808D'} size={20} />;
+        }}
+      />
         <TextInput 
         onChangeText={(text)=> setNewItem({...newItem, Observacion: text})}
         style={styles.inputObservaciones}placeholder="Observaciones"
@@ -163,22 +350,49 @@ const styles = StyleSheet.create({
         width: 200, 
         height: 200, 
       },
-      pickerContainer: {
+      dropdownButtonStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         width: 250,
-        height: 50,
-        borderRadius: 10,
-        overflow: 'hidden',
+        height: 43,
         borderColor: '#fff',
         borderWidth: 2,
+        borderRadius: 10,
+        padding: 10,
         backgroundColor: '#ffffff90',
+        fontWeight: '400',
+        marginTop: 10,
       },
-      picker: {
-        color: 'gray', 
 
+      dropdownItemStyle: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        backgroundColor: '#ffffff90',
+        width: 300,
+        borderColor: '#fff',
+        borderWidth: 1,
       },
-      pickerItem: {
-        color: 'black',
-        fontSize: 15,
+      dropdownItemIconStyle: {
+        fontSize: 16,
+        color: '#000',
+        marginRight: 10,
+      },
+      dropdownItemTxtStyle: {
+        fontSize: 16,
+        color: '#000',
+      },
+      dropdownMenuStyle: {
+        position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: [{ translateX: -150 }, { translateY: -150 }],
+      width: 300,
+      zIndex: 1000,
+      height: 500,
+        borderRadius: 5,
+        backgroundColor: '#ffffff90',
       },
       inputContainer: {
         width: 250,
